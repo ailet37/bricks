646a1fe313da5d588fb7ab0985983202a88b126d
ailet <alina.rudnik@gmail.com>
2017-10-01 15:56:50 +0300

    services, partners, reviews, seo

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,44 @@
  var gulp = require('gulp'),
- rigger = require('gulp-rigger'),
- browserSync = require('browser-sync'),
- sass = require('gulp-sass'),
- sourcemaps = require('gulp-sourcemaps'),
- autoprefixer = require('gulp-autoprefixer'),
- cssmin = require('gulp-minify-css'),
- svgo = require('gulp-svgo'),
- imagemin     = require('gulp-imagemin'),
- cache = require('gulp-cache'),
- plumber = require('gulp-plumber');
+     browserSync = require('browser-sync'),
+     sass = require('gulp-sass'),
+     sourcemaps = require('gulp-sourcemaps'),
+     autoprefixer = require('gulp-autoprefixer'),
+     cssmin = require('gulp-minify-css'),
+     imagemin     = require('gulp-imagemin'),
+     cache = require('gulp-cache'),
+     newer = require('gulp-newer'),
+     plumber = require('gulp-plumber');
 
  var path = {
      build: { //Тут мы укажем куда складывать готовые после сборки файлы
-         html: 'build/',
          js: 'js/',
          css: '',
          img: 'assets/img/',
-         svg: 'assets/svg/',
-         fonts: 'assets/fonts/'
+         svg: 'assets/svg/'
      },
      src: { //Пути откуда брать исходники
-         html: 'dev/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-         js: 'dev/js/script.js',//В стилях и скриптах нам понадобятся только main файлы
+         js: 'dev/js/script.js',
          scss: 'style.scss',
          img: 'dev/assets/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
-         svg: 'dev/assets/svg/**/*.*',
-         fonts: 'dev/assets/fonts/**/*.*'
+         svg: 'dev/assets/svg/**/*.*'
      },
      watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-         html: 'dev/**/*.html',
          js: 'dev/js/*.js',
          scss: 'css/**/*.scss',
          img: 'dev/assets/img/**/*.*',
          svg: 'dev/assets/svg/**/*.*',
-         fonts: 'dev/assets/fonts/**/*.*'
      },
      clean: './build'
  };
 
  var config = {
-     server: {
-         baseDir: "./build"
-     },
-     tunnel: true,
-     host: 'localhost',
-     port: 8888,
-     logPrefix: "Frontend_Bricks"
+     //proxy: 'bricks.dev',
+     proxy: "localhost:8888",
+     //tunnel: true,
+     //host: 'localhost',
+     //port: 8888,
+     //logPrefix: "Frontend_Bricks",
+     injectChanges: true
  };
 
 
@@ -54,21 +47,13 @@
  });
 
 
- gulp.task('html:build', function () {
-     gulp.src(path.src.html) //Выберем файлы по нужному пути
-         .pipe(rigger()) //Прогоним через rigger
-         .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
-         .pipe(browserSync.reload({stream: true})); //И перезагрузим наш сервер для обновлений
- });
-
-
- gulp.task('scss:build', function () {
+ gulp.task('scss', function () {
      gulp.src(path.src.scss) //Выберем наш main.scss
          .pipe(plumber())
          .pipe(sourcemaps.init())
          .pipe(sass()) //Скомпилируем
          .pipe(autoprefixer()) //Добавим вендорные префиксы
-         //.pipe(cssmin()) //Сожмем
+         .pipe(cssmin()) //Сожмем
          .pipe(sourcemaps.write())
          .pipe(gulp.dest(path.build.css)) //И в build
          .pipe(browserSync.reload({stream: true}));
@@ -96,50 +81,37 @@
  /**
   * Images
   *
-  * Look at src/images, optimize the images and send them to the appropriate place
+  * Look at assets/img, optimize the images and send them to the appropriate place
   */
  gulp.task('images', function() {
 
 // Add the newer pipe to pass through newer images only
      return 	gulp.src(path.src.img)
-         //.pipe(newer('./assets/img/'))
+         .pipe(newer(path.build.img))
          ///.pipe(rimraf({ force: true }))
+
          .pipe(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
          .pipe(gulp.dest(path.build.img))
  });
 
 
  gulp.task('svg', function() {
-     gulp.src(path.src.svg)
+     return gulp.src(path.src.svg)
+         .pipe(imagemin())
          .pipe(gulp.dest(path.build.svg))
  });
 
- gulp.task('fonts:build', function() {
-     gulp.src(path.src.fonts)
-         .pipe(gulp.dest(path.build.fonts))
- });
 
  gulp.task('clear', function () {
      cache.clearAll();
  });
 
 
-
- gulp.task('build', [
-     'html:build',
-     'js:build',
-     'scss:build',
-     'fonts:build',
-     'svg',
-     'images'
- ]);
-
-
  gulp.task('watch', function(){
-     gulp.watch('css/*.scss', ['scss:build']);
-     gulp.watch('*.scss', ['scss:build']);
-     gulp.watch('dev/blocks/**/*.scss', ['scss:build']);
-     gulp.watch(path.watch.js, ['js:build']);
+     gulp.watch('css/**/*.scss', ['scss']);
+     gulp.watch('./*.scss', ['scss']);
+
+     gulp.watch(path.watch.js, ['js']);
      gulp.watch(path.watch.svg, ['svg']);
      gulp.watch(path.watch.img, ['images']);
      // Other watchers
