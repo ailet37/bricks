615b07e7468b8a1d7c1e3fb63fb112dea74ca590
ailet <alina.rudnik@gmail.com>
2017-10-03 23:33:27 +0300

    ...

---
+++ category.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Category Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="category-header">
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="/" class="breadcrumbs__item">Главная</a>
+            <a href="" class="breadcrumbs__item _active">Химические средства для бассейнов</a>
+        </div>
+        <div class="category-header__title title-md">Химические средства для бассейнов</div>
+        <div class="category-header__text">
+            Обслуживание домашнего бассейна либо бассейна общественного пользования представляет собой довольно сложную и дорогостоящую процедуру. Поэтому для покупателей важно найти такой магазин химии для бассейнов, который предлагает не только высокое качество, но и приемлемые цены.
+        </div>
+    </div>
+</section>
+
+<section class="category">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-lg-3">
+                <div class="category__item">
+                    <div class="category__img">пик</div>
+                    <div class="category__title">Название</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
