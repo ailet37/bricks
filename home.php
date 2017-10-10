2d3a366645c6175420e76d097b9121a1b0674c13
ailet <alina.rudnik@gmail.com>
2017-10-10 22:54:06 +0300

    sauna page + fixes

--- home.php
+++ home.php
@@ -8,118 +8,119 @@ Template Name: Front Page
 
 
 <section class="banner">
-        <div class="swiper-container banner__wrap js-banner">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide banner__cover">
-                    <div class="container">
-                        <div class="banner__info">
-                            <div class="banner__title">
-                                Строительство
-                                бассейнов
-                                хамамов, саун
-                            </div>
-                            <div class="banner__descr">
-                                Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов
-                            </div>
-                            <div class="banner__btn btn">узнать больше</div>
+    <div class="swiper-container banner__wrap js-banner">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide banner__cover">
+                <div class="container">
+                    <div class="banner__info">
+                        <div class="banner__title">
+                            Строительство
+                            бассейнов
+                            хамамов, саун
                         </div>
-                    </div>
-                </div>
-                <div class="swiper-slide banner__cover">
-                    <div class="container">
-                        <div class="banner__info">
-                            <div class="banner__title">
-                                Строительство
-                                бассейнов
-                                хамамов, саун
-                            </div>
-                            <div class="banner__descr">
-                                Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов
-                            </div>
-                            <div class="banner_btn btn">узнать больше</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="js-banner-pag"></div>
-        </div>
-    </section>
-
-<section class="services">
-        <div class="container">
-            <div class="row gutters">
-                <div class="col-xs-12 col-sm-12 col-lg-4">
-                    <div class="services__description">
-                        <h1 class="services__title title-md">Наши услуги</h1>
-                        <div class="services__text text-md">
-                            Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов.
+                        <div class="banner__descr">
                             Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов
                         </div>
+                        <div class="banner__btn btn">узнать больше</div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-lg-8">
-                    <a href="" class="cart-item cart-item_lg"
-                       style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-1.jpg);">
-                        <div class="cart-item__title">
-                            Строительство бассейнов
-                        </div>
-                        <div class="cart-item__btn center-svg">
-                            <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-4 col-lg-4">
-                    <a href="" class="cart-item"
-                       style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-2.jpg);">
-                        <div class="cart-item__title">
-                            Строительство хамамов и бань
-                        </div>
-                        <div class="cart-item__btn center-svg">
-                            <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-4 col-lg-4">
-                    <div class="cart-item"
-                         style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-3.jpg);">
-                        <div class="cart-item__title">
-                            Ремонт
-                            и обслуживание
+            </div>
+            <div class="swiper-slide banner__cover">
+                <div class="container">
+                    <div class="banner__info">
+                        <div class="banner__title">
+                            Строительство
                             бассейнов
+                            хамамов, саун
                         </div>
-                        <div class="cart-item__btn center-svg">
-                            <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                        <div class="banner__descr">
+                            Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов
                         </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-4 col-lg-4">
-                    <div class="cart-item"
-                         style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-4.jpg);">
-                        <div class="cart-item__title">
-                            Химические
-                            средства для
-                            бассейнов
-                        </div>
-                        <div class="cart-item__btn center-svg">
-                            <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
-                        </div>
+                        <div class="banner_btn btn">узнать больше</div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="js-banner-pag"></div>
+    </div>
+</section>
+
+<section class="services">
+    <div class="container">
+        <div class="row gutters">
+            <div class="col-xs-12 col-sm-12 col-lg-4">
+                <div class="services__description">
+                    <h1 class="services__title title-md">Наши услуги</h1>
+                    <div class="services__text text-md">
+                        Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов.
+                        Обслуживание и ремонт бассейнов, хамамов, саун и SPA. Оборудование для бассейнов
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-lg-8">
+                <a href="" class="cart-item cart-item_lg"
+                   style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-1.jpg);">
+                    <div class="cart-item__title">
+                        Строительство бассейнов
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-lg-4">
+                <a href="" class="cart-item"
+                   style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-2.jpg);">
+                    <div class="cart-item__title">
+                        Строительство хамамов и бань
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-lg-4">
+                <div class="cart-item"
+                     style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-3.jpg);">
+                    <div class="cart-item__title">
+                        Ремонт
+                        и обслуживание
+                        бассейнов
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-lg-4">
+                <div class="cart-item"
+                     style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/services/services-4.jpg);">
+                    <div class="cart-item__title">
+                        Химические
+                        средства для
+                        бассейнов
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <section class="seo-post">
-        <div class="container">
-            <h1 class="seo-post__title title-md">Какой-то заголовок</h1>
-            <div class="seo-post__text">
-                По их почти единодушному мнению, действие существенно представляет собой ролевой контраст. Конечно,
-                нельзя не принять во внимание тот факт, что самость возможна.
-                <br><br>
-                Все это побудило нас обратить внимание на то, что действие мгновенно выбирает ролевой эскапизм.
-                Проекция возможна. Сновидение важно представляет собой кризис.
-            </div>
-    </section>
+    <div class="container">
+        <h1 class="seo-post__title title-md">Какой-то заголовок</h1>
+        <div class="seo-post__text">
+            По их почти единодушному мнению, действие существенно представляет собой ролевой контраст. Конечно,
+            нельзя не принять во внимание тот факт, что самость возможна.
+            <br><br>
+            Все это побудило нас обратить внимание на то, что действие мгновенно выбирает ролевой эскапизм.
+            Проекция возможна. Сновидение важно представляет собой кризис.
+        </div>
+    </div>
+</section>
 
 <?php get_template_part('template-parts/partners'); ?>
 <?php get_template_part('template-parts/reviews'); ?>
--- sauna.php
+++ sauna.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Pools Page
+Template Name: Sauna Page
 */
 ?>
 <?php get_header(); ?>
@@ -11,7 +11,7 @@ Template Name: Pools Page
         <div class="teaser__info">
             <div class="teaser__title title-md">
                 Строительство
-                бассейнов
+                бань, саун, хамамов
             </div>
             <div class="teaser__descr">
                 Обслуживание домашнего бассейна либо бассейна общественного пользования представляет собой довольно
@@ -28,52 +28,29 @@ Template Name: Pools Page
         <div class="advantages__row row">
             <div class="col-xs-12 col-sm-6 col-lg-6">
                 <div class="advantages__about">
-                    <h2 class="advantages__title title-md">Строительство
-                        бассейнов</h2>
-                    <div class="advantages__main-text text-md">Вы сможете приобрести у нас современные препараты для
-                        бассейнов всех типов, включая эффективные средства на основе активного кислорода и хлора.
+                    <h2 class="advantages__title title-md">
+                        Бани из бруса
+                    </h2>
+                    <div class="advantages__main-text text-md">
+                        Вы сможете приобрести у нас современные препараты для бассейнов всех типов, включая эффективные
+                        средства на основе активного кислорода и хлора.
                         Кроме средств для дезинфекции и очистки воды, наш магазин предлагает препараты для уничтожения
                     </div>
                 </div>
 
             </div>
             <div class="col-xs-12 col-sm-6 col-lg-6">
-                <div class="advantages__wrap">
-                    <div class="advantages__line">
-                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/advantages/adv-1.svg'); ?>
-                        <div class="advantages__text">
-                            Строительство открытых бассейнов, сборка и установка павильонов, плавающих покрывал и
-                            жалюзей для них
-                        </div>
-                    </div>
-                    <div class="advantages__line">
-                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/advantages/adv-3.svg'); ?>
-                        <div class="advantages__text">
-                            Строительство бассейнов в домах
-                        </div>
-                    </div>
-                    <div class="advantages__line">
-                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/advantages/adv-2.svg'); ?>
-                        <div class="advantages__text">
-                            Бассейны в рамках строительства банного комплекса
-                        </div>
-                    </div>
+                <div class="advantages__img">
+                    <img src="<?php bloginfo("template_directory"); ?>/assets/img/sauna/sauna-1.jpg" alt="">
                 </div>
             </div>
+
         </div>
         <div class="advantages__row row">
-
-            <div class="col-xs-12 col-sm-6 col-lg-6">
-                <div class="advantages__img">
-                    <img src="<?php bloginfo("template_directory"); ?>/assets/img/pools/pools-3.jpg" alt="">
-
-                </div>
-            </div>
             <div class="col-xs-12 col-sm-6 col-lg-6">
                 <div class="advantages__about">
                     <h2 class="advantages__title title-md">
-                        Стационарные
-                        бассейны
+                        Бани из бревна
                     </h2>
                     <div class="advantages__main-text text-md">Вы сможете приобрести у нас современные препараты для
                         бассейнов всех типов, включая эффективные средства на основе активного кислорода и хлора.
@@ -81,12 +58,20 @@ Template Name: Pools Page
                     </div>
                 </div>
             </div>
+            <div class="col-xs-12 col-sm-6 col-lg-6">
+                <div class="advantages__img">
+                    <img src="<?php bloginfo("template_directory"); ?>/assets/img/sauna/sauna-2.jpg" alt="">
+                </div>
+            </div>
+
         </div>
         <div class="advantages__row row">
             <div class="col-xs-12 col-sm-6 col-lg-6">
                 <div class="advantages__about">
                     <h2 class="advantages__title title-md">
-                        Сборные бассейны
+                        Бани из
+                        оцилиндрованного
+                        бревна
                     </h2>
                     <div class="advantages__main-text">Вы сможете приобрести у нас современные препараты для бассейнов
                         всех типов, включая эффективные средства на основе активного кислорода и хлора.
@@ -96,8 +81,53 @@ Template Name: Pools Page
             </div>
             <div class="col-xs-12 col-sm-6 col-lg-6">
                 <div class="advantages__img">
-                    <img src="<?php bloginfo("template_directory"); ?>/assets/img/pools/pools-3.jpg" alt="">
+                    <img src="<?php bloginfo("template_directory"); ?>/assets/img/sauna/sauna-3.jpg" alt="">
 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="services sauna-types">
+    <div class="container">
+        <h2 class="sauna-types__title title-md">Какие типы бань мы делаем?</h2>
+        <div class="row gutters">
+
+            <div class="col-xs-12 col-sm-4 col-lg-4">
+                <a href="" class="cart-item"
+                   style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/sauna/fin.jpg);">
+                    <div class="cart-item__title">
+                        Финская
+                        парная
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-lg-4">
+                <div class="cart-item"
+                     style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/sauna/turk.jpg);">
+                    <div class="cart-item__title">
+                        Турецкая
+                        сауна
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-lg-4">
+                <div class="cart-item"
+                     style="background-image: url(<?php bloginfo("template_directory"); ?>/assets/img/sauna/rus.jpg);">
+                    <div class="cart-item__title">
+                        Русская
+                        баня
+                    </div>
+                    <div class="cart-item__btn circle-btn center-svg">
+                        <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/arrow.svg'); ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -107,7 +137,6 @@ Template Name: Pools Page
 <section class="calculate">
     <div class="container">
         <div class="calculate__inner">
-
 
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-lg-5">
@@ -121,20 +150,7 @@ Template Name: Pools Page
                 </div>
                 <div class="col-xs-12 col-sm-12 col-lg-7">
                     <form class="calculate__form">
-                        <div class="calculate__form-row row">
-                            <div class="col-xs-12 col-sm-6">
-                                <input id="after" type="text" class="input calculate__form-input"
-                                       placeholder="Длина,см">
-                                <input type="text" class="input calculate__form-input" placeholder="Ширина,см">
-                                <input type="text" class="input calculate__form-input" placeholder="Глубина,см">
-                            </div>
-                            <div class="col-xs-12 col-sm-6">
-                                <select name="" id="" class="select">
-                                    <option value="">Выберите материал</option>
-                                    <option value="">материальчик</option>
-                                </select>
-                            </div>
-                        </div>
+
                         <div class="calculate__form-row row">
                             <div class="col-xs-12 col-sm-6">
                                 <input type="text" class="input" placeholder="Ваше имя">
@@ -161,57 +177,6 @@ Template Name: Pools Page
     </div>
 </section>
 
-<section class="steps">
-    <div class="container">
-        <h2 class="steps__title title-md">Примерные этапы
-            строительства бассейна
-        </h2>
-        <div class="steps__wrap row">
-            <div class="steps__row col-xs-12 col-sm-4 col-lg-2">
-                <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/steps/steps-project.svg'); ?>
-                <div class="steps__text">
-                    Проектирование бассейна
-                </div>
-            </div>
-            <div class="steps__row col-xs-12 col-sm-4 col-lg-2">
-                <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/steps/steps-prepare.svg'); ?>
-                <div class="steps__text">
-                    Подготовка основания
-                </div>
-            </div>
-            <div class="steps__row col-xs-12 col-sm-4 col-lg-2">
-                <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/steps/steps-pipes.svg'); ?>
-                <div class="steps__text">
-                    Укладка закладных
-                    элементов
-                </div>
-            </div>
-            <div class="steps__row col-xs-12 col-sm-4 col-lg-2">
-                <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/steps/steps-armin.svg'); ?>
-                <div class="steps__text">
-                    Сооружение опалубки
-                    и армирование
-                    котлована
-                </div>
-            </div>
-            <div class="steps__row col-xs-12 col-sm-4 col-lg-2">
-                <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/steps/steps-isolation.svg'); ?>
-                <div class="steps__text">
-                    Устройство
-                    гидроизоляции
-                    и бетонирование
-                </div>
-            </div>
-            <div class="steps__row col-xs-12 col-sm-4 col-lg-2">
-                <?php echo file_get_contents(get_template_directory_uri() . '/assets/svg/steps/steps-finishing.svg'); ?>
-                <div class="steps__text">
-                    Отделочные работы
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
 <section class="seo-post">
     <div class="container">
 
@@ -231,3 +196,4 @@ Template Name: Pools Page
 
 
 <?php get_footer(); ?>
+
